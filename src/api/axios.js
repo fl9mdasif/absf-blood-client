aaf14eb82fc9d43bacbd4b51737db32a0ef5752a
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2025-07-30 18:46:46 +0600

    added the live server site link

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a new axios instance with a base URL
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Your backend server URL
+    baseURL: 'https://absf-bloodhub-server.vercel.app/api', // Your backend server URL
     headers: {
         'Content-Type': 'application/json',
     },
